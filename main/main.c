f662d96bd4d5a4967815b96eb096c2d4c8ff00d3
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2022-01-30 16:38:57 -0300

    Ajustes na exibição

--- main/main.c
+++ main/main.c
@@ -1,13 +1,3 @@
-/* WiFi station Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
