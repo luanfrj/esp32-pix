b377a8bba604a83c9f673232eec505d266fa4e6e
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2022-02-15 19:37:04 -0300

    Ajuste no tratamento de eventos HTTP

--- main/http_client.c
+++ main/http_client.c
@@ -47,9 +47,6 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
         case HTTP_EVENT_ON_FINISH:
             ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
             if (output_buffer != NULL) {
-                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
-                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
-                ESP_LOGI(TAG, "Limpando Buffer");
                 free(output_buffer);
                 output_buffer = NULL;
             }
@@ -60,21 +57,14 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
             int mbedtls_err = 0;
             esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
             if (err != 0) {
-                ESP_LOGE(TAG, "ERRO");
-                if (output_buffer != NULL) {
-                    ESP_LOGI(TAG, "Limpando Buffer");
-                    free(output_buffer);
-                    output_buffer = NULL;
-                }
-                output_len = 0;
                 ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
                 ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
             }
             if (output_buffer != NULL) {
-                ESP_LOGI(TAG, "Limpando Buffer");
                 free(output_buffer);
                 output_buffer = NULL;
             }
+            output_len = 0;
             break;
     }
     return ESP_OK;
