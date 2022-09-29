14cf7fb54ae730f170fc6274eb19e9097c12b0eb
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2022-09-28 22:17:04 -0300

    Correções na compilação da bibioteca de comunicação com o cartão sd

--- components/sdcard/sdcard.h
+++ components/sdcard/sdcard.h
@@ -11,4 +11,12 @@
 #define PIN_NUM_CLK   CONFIG_PIN_CLK
 #define PIN_NUM_CS    CONFIG_PIN_CS
 
+#if CONFIG_IDF_TARGET_ESP32S2
+#define SPI_DMA_CHAN    host.slot
+#elif CONFIG_IDF_TARGET_ESP32C3
+#define SPI_DMA_CHAN    SPI_DMA_CH_AUTO
+#else
+#define SPI_DMA_CHAN    1
+#endif
+
 void sdcard_init(sdmmc_card_t *card);
